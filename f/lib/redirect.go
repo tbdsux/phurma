bacff8b0feb60c9cf524140b272b325fd17e9584
tbdsux <iamcoderx@gmail.com>
2023-04-03 10:30:14 +0800

    some fixes and minor changes
    - added copy button on usage's codeblocks (form url and code)
    - fixed redirects on `/f` form apis
    - updated demo

--- f/lib/redirect.go
+++ f/lib/redirect.go
@@ -5,17 +5,9 @@ import "os"
 var IS_DEVELOPMENT = os.Getenv("DEVELOPMNET") == "true"
 
 func RedirectFilesNotAllowed() string {
-	if IS_DEVELOPMENT {
-		return "http://localhost:3000/files-not-allowed"
-	}
-
 	return "/files-not-allowed"
 }
 
 func RedirectThankYou() string {
-	if IS_DEVELOPMENT {
-		return "http://localhost:3000/thank-you"
-	}
-
 	return "/thank-you"
 }
--- phurma/src/modules/forms/usage.tsx
+++ phurma/src/modules/forms/usage.tsx
@@ -1,33 +1,8 @@
+import { ClipboardDocumentIcon } from "@heroicons/react/20/solid";
 import { useForms } from "./context";
 
-const FormUsage = () => {
-  const { form } = useForms();
-
-  return (
-    <div className="mt-8 md:mx-8">
-      <h4 className="text-xl font-bold text-gray-800">How to use?</h4>
-
-      <ul className="mt-2 md:mx-8">
-        <li className="my-3">
-          <p className="text-gray-700">1. Copy your form url</p>
-          <pre className="mt-1 w-full overflow-auto rounded-xl bg-rose-100 py-2 px-4 text-sm text-gray-700">
-            {form?.url}
-          </pre>
-        </li>
-
-        <li className="my-3">
-          <p className="text-gray-700">2. Add your url to your html form</p>
-          <p className="text-gray-700">
-            You need to set the{" "}
-            <code className="rounded-xl bg-rose-100 py-0.5 px-1 text-xs text-gray-700 ">
-              enctype
-            </code>{" "}
-            property in your form correctly so that the app knows how to parse
-            responses.
-          </p>
-          <pre className="mt-1 w-full overflow-auto rounded-xl bg-rose-100 py-1 px-4 text-sm text-gray-700">
-            {`
-<form action="${form?.url}" method="post" enctype="multipart/form-data">
+const generateFormCode = (formUrl: string) => `
+<form action="${formUrl}" method="post" enctype="multipart/form-data">
 <div>
     <label for="name">Name</label>
     <input type="text" name="name" id="name" />
@@ -45,15 +20,71 @@ const FormUsage = () => {
 
 <button type="submit">submit</button>
 </form>
-            `}
+            `;
+
+const FormUsage = () => {
+  const { form } = useForms();
+
+  const copyUrl = () => {
+    if (!form) return;
+
+    navigator.clipboard.writeText(form.url);
+  };
+
+  const copyFormCode = () => {
+    if (!form) return;
+
+    navigator.clipboard.writeText(generateFormCode(form.url));
+  };
+
+  return (
+    <div className="mt-8 md:mx-8">
+      <h4 className="text-xl font-bold text-gray-800">How to use?</h4>
+
+      <ul className="mt-2 md:mx-8">
+        <li className="my-3">
+          <p className="text-gray-700">1. Copy your form url</p>
+          <pre className="relative mt-1 w-full overflow-auto rounded-xl bg-rose-100 py-2 px-4 text-sm text-gray-700">
+            {form?.url}
+
+            <button
+              onClick={copyUrl}
+              className="absolute top-2 right-2"
+              title="Copy"
+            >
+              <ClipboardDocumentIcon aria-hidden="true" className="h-4 w-4" />
+            </button>
+          </pre>
+        </li>
+
+        <li className="my-3">
+          <p className="text-gray-700">2. Add your url to your html form</p>
+          <p className="text-gray-700">
+            You need to set the{" "}
+            <code className="rounded-xl bg-rose-100 py-0.5 px-1 text-xs text-gray-700 ">
+              enctype
+            </code>{" "}
+            property in your form correctly so that the app knows how to parse
+            responses.
+          </p>
+          <pre className="relative mt-1 w-full overflow-auto rounded-xl bg-rose-100 py-1 px-4 text-sm text-gray-700">
+            <button
+              onClick={copyFormCode}
+              className="absolute top-2 right-2"
+              title="Copy"
+            >
+              <ClipboardDocumentIcon aria-hidden="true" className="h-4 w-4" />
+            </button>
+
+            {generateFormCode(form ? form.url : "")}
           </pre>
         </li>
 
         <li className="my-3">
           <p className="text-gray-700">3. View submissions in your dashboard</p>
           <p className="text-gray-700">
-            You can now view all of the responses or submissions sent in your
-            form.
+            You can now view all of the responses or submissions sent in the{" "}
+            <strong>View Responses</strong> tab.
           </p>
         </li>
       </ul>
